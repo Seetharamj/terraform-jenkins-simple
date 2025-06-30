50f9d65067668f2354ce41faad4bcc02907fb07a
seetharam j <seetharamj704@gmail.com>
2025-06-30 22:13:52 +0530

    Update jenkinsfile.groovy

--- jenkins-pipeline/jenkinsfile.groovy
+++ jenkins-pipeline/jenkinsfile.groovy
@@ -9,7 +9,7 @@ pipeline {
   stages {
     stage('Clone Repo') {
       steps {
-        git branch: 'main', url: 'https://github.com/yourusername/terraform-jenkins-simple.git'
+        git branch: 'main', url: 'https://github.com/seetharamj/terraform-jenkins-simple.git'
       }
     }
 
